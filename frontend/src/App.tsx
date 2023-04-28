c329dcd3c816e44cef6183f8637e172b6a27db2f
Iker Ocio <ikerocio@yahoo.es>
2023-04-28 17:19:37 +0200

    Sidebar added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,26 +3,28 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 import LoginForm from "./components/Login";
-
+import Sidebar from "./components/Siderbar";
 import ChatWindow from "./components/ChatWindow";
 
 function App() {
   return (
+    <Sidebar />
+    /*
     <div className="flex h-screen">
       <div className="w-100 min-w-100 bg-gray-900">
-        {/* Navbar de 100px de ancho */}
+        }
         <div className="w-chat-window">
           <LoginForm />
         </div>
       </div>
       <div className="flex-1 max-w-screen-xl m-auto">
-        {/* Contenido principal con ancho máximo de pantalla */}
         <div className="w-chat-window">
           <ChatWindow />
         </div>
       </div>
       <ToastContainer />
     </div>
+    */
   );
 }
 
