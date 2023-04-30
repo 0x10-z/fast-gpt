63ad5992bd24cd18ba33378edd992e0c14136c3c
Iker Ocio <iker@ikerocio.com>
2023-04-30 23:02:03 +0200

    Improved

--- frontend/src/components/MessagesList.tsx
+++ frontend/src/components/MessagesList.tsx
@@ -16,47 +16,50 @@ interface MessagesListProps {
 function MessagesList({ loading, messages, lastMessageRef }: MessagesListProps) {
   useEffect(() => {
     hljs.highlightAll();
-  });
+  }, []);
  
   return (
-
-    <div className="flex flex-col space-y-4 w-full " id="scroll-container">
-  {messages.map((message, index) => (
-    <div className={` ${
-      message.sender === "user" ? "user bg-gray-200" : "assistant"
-    }`}>
-      <div
-        key={index}
-        custom-timestamp={message.timestamp}
-        className={`flex flex-row justify-center py-4 w-full max-w-[700px] mx-auto my-5 h-auto`}>
-        <div className="pb-2 w-10 flex flex-row items-start mx-5">
-          <img
-            className="ring-2 ring-black w-full object-cover"
-            alt={message.sender === "user" ? "user avatar" : "chatgpt avatar"}
-            src={message.sender === "user" ? "user.png" : "chatgpt.png"}
-          />
-        </div>
+    <div className="flex flex-col space-y-4 w-full" id="scroll-container">
+      {messages.map((message, index) => (
         <div
-          ref={lastMessageRef}
-          className="flex justify-start items-center flex bg-gray w-full h-full"
+          key={index}
+          className={`${
+            message.sender === "user" ? "user bg-gray-200" : "assistant"
+          }`}
         >
-          {index === messages.length - 1 && loading ? (
-            <div className="flex mb-10 justify-center items-center bg-gray-300 bg-opacity-50">
-              {<Loader />}
+          <div
+            custom-timestamp={message.timestamp}
+            className="flex flex-row justify-center py-4 w-full max-w-[700px] mx-auto my-5 h-auto"
+          >
+            <div className="pb-2 w-10 flex flex-row items-start mx-5">
+              <img
+                className="ring-2 ring-black w-full object-cover"
+                alt={
+                  message.sender === "user" ? "user avatar" : "chatgpt avatar"
+                }
+                src={message.sender === "user" ? "user.png" : "chatgpt.png"}
+              />
             </div>
-          ) : (
-            <div className="w-full mr-10">
-              <ReactMarkdown children={message.content} />
+            <div
+              ref={index === messages.length - 1 ? lastMessageRef : null}
+              className="flex justify-start items-center flex bg-gray w-full h-full"
+            >
+              {index === messages.length - 1 && loading ? (
+                <div className="flex mb-10 justify-center items-center bg-gray-300 bg-opacity-50">
+                  <Loader />
+                </div>
+              ) : (
+                <div className="w-full mr-10">
+                  <ReactMarkdown children={message.content} />
+                </div>
+              )}
             </div>
-          )}
+          </div>
         </div>
-      </div>
+      ))}
     </div>
-  ))}
-</div>
-
-
   );
 }
+
 
 export default MessagesList;
